4d293cf5c899957e3ed871dd957ee60d0f61db11
iSymphony Inc <qhuynh@isymphony.net>
2026-01-26 09:56:28 -0600

    removed iOS menu: make text more compact, moved to global.css

--- src/components/AppMenu.vue
+++ src/components/AppMenu.vue
@@ -457,12 +457,4 @@ const logout = async () => {
 .menu-bottom-spacer {
   height: 50px;
 }
-
-/* iOS menu: make text more compact */
-:host(.ios) ion-item ion-label {
-  font-size: 0.92rem;      /* ~8% smaller */
-  line-height: 1.2;
-  font-weight: 500;
-  white-space: nowrap;    /* prevent wrapping */
-}
 </style>
