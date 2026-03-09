c7b0bb75b1d2701fb19234e67439df8e343f47ff
iSymphony Inc <qhuynh@isymphony.net>
2026-03-09 09:22:30 -0500

    Update API configuration to enable TEST SERVER and disable OFFICE DEV URL

--- src/config/api.config.example.ts
+++ src/config/api.config.example.ts
@@ -2,10 +2,10 @@
 //const APP_API = "http://192.168.160.19/isymphony_api/public/api";
 
 /* OFFICE DEV */
-const APP_API = "http://10.1.10.33/isymphony_api/public/api";
+//const APP_API = "http://10.1.10.33/isymphony_api/public/api";
 
 /* TEST SERVER */
-//const APP_API = "http://newdevmobile.isymphony.net/api";
+const APP_API = "https://newdevmobile.isymphony.net/api";
 
 /* PRODUCTION */
 //const APP_API = "https://newpromobile.isymphony.net/api";
