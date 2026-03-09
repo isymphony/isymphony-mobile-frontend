340b46ebc0e400893f117192a1b93cf170795bc2
iSymphony Inc <qhuynh@isymphony.net>
2026-03-08 23:11:57 -0500

    Add API configuration for different environments in api.config.example.ts

--- src/config/api.config.example.ts
+++ src/config/api.config.example.ts
@@ -1,2 +1,18 @@
-export const LOCAL_IP = "192.168.1.100";
-export const API_TIMEOUT = 15000;
+/* HOME DEV */
+//const APP_API = "http://192.168.160.19/isymphony_api/public/api";
+
+/* OFFICE DEV */
+const APP_API = "http://10.1.10.33/isymphony_api/public/api";
+
+/* TEST SERVER */
+//const APP_API = "http://newdevmobile.isymphony.net/api";
+
+/* PRODUCTION */
+//const APP_API = "https://newpromobile.isymphony.net/api";
+
+const API_TIMEOUT = 15000;
+
+export {
+  APP_API,
+  API_TIMEOUT
+};
